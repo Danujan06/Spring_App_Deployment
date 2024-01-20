01e66b662ba2b93357d4f3dd966d1bb07d32bbb3
Danujan <sivasundaralingamdanujan@gmail.com>
2024-01-20 18:07:00 +0530

    Configurations Changed

--- src/main/java/com/group15/AutomatedHydroponicsSystem/Demo/TestController.java
+++ src/main/java/com/group15/AutomatedHydroponicsSystem/Demo/TestController.java
@@ -1,6 +1,7 @@
 package com.group15.AutomatedHydroponicsSystem.Demo;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.Map;
 
 @RestController
 public class TestController {
+    @GetMapping
     public Object hello(){
         Map<String, String> object = new HashMap<>();
         object.put("name","Danujan");
